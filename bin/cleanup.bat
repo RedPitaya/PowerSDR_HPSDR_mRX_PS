47515c268494062ce1e83d9199687572ad66aed7
TheChief <thechief@praised-land.de>
2017-02-20 22:01:17 +0100

    DG8MG: Added batch files to Git
    Signed-off-by: TheChief <thechief@praised-land.de>

---
+++ bin/cleanup.bat
@@ -0,0 +1,10 @@
+del /s ..\*.pdb
+del /s ..\*.iobj
+del /s ..\*.ipdb
+del /s ..\*.obj
+del /s ..\*.log
+del /s ..\*.tlog
+del /s /q ..\temp\*.*
+del /s /f ..\desktop.ini
+pause
+
---
+++ bin/test.bat
@@ -0,0 +1,3 @@
+if not "%cd%" == "C:\Users\Grundner\Google Drive\Charly 25\AA Red Pitaya_Hamlab with C25_10_80_10W (includes former K Software)\K Software\PowerSDR_HPSDR_mRX_PS-C25_Master\bin" exit /b
+echo "Ausfuehrung ist okay"
+pause
