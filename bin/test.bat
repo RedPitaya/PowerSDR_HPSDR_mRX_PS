c097ac1eeb6cafffbf32713fd3f4de44ddc25a42
TheChief <thechief@praised-land.de>
2017-06-17 23:03:55 +0200

    DG8MG: Cleaned up and updated the project repository with the latest files from the OpenHPSDR-PowerSDR project.
    Signed-off-by: TheChief <thechief@praised-land.de>

--- bin/test.bat
+++
@@ -1,3 +0,0 @@
-if not "%cd%" == "C:\Users\Grundner\Google Drive\Charly 25\AA Red Pitaya_Hamlab with C25_10_80_10W (includes former K Software)\K Software\PowerSDR_HPSDR_mRX_PS-C25_Master\bin" exit /b
-echo "Ausfuehrung ist okay"
-pause
