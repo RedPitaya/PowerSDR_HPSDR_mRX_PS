7989e517ffbff2ea6b1afb1cd64f52096c38d264
TheChief <thechief@praised-land.de>
2017-07-20 16:34:10 +0200

    DG8MG: Commit of the new master build for customer release with the new HAMlab 160-6.
    Signed-off-by: TheChief <thechief@praised-land.de>

---
+++ Source/Console/C25SWRView.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace PowerSDR
+{
+    partial class C25SWRViewForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(C25SWRViewForm));
+            this.chrC25SWRView = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chrC25SWRView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chrC25SWRView
+            // 
+            this.chrC25SWRView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.AxisX.IsStartedFromZero = false;
+            chartArea1.AxisX.MinorTickMark.Enabled = true;
+            chartArea1.AxisX.ScaleView.Zoomable = false;
+            chartArea1.AxisX.Title = "Frequency (MHz)";
+            chartArea1.AxisY.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.AxisY.MinorTickMark.Enabled = true;
+            chartArea1.AxisY.Title = "SWR";
+            chartArea1.Name = "chaC25SWRView";
+            this.chrC25SWRView.ChartAreas.Add(chartArea1);
+            this.chrC25SWRView.Location = new System.Drawing.Point(12, 12);
+            this.chrC25SWRView.Name = "chrC25SWRView";
+            series1.ChartArea = "chaC25SWRView";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Name = "serSWRData";
+            this.chrC25SWRView.Series.Add(series1);
+            this.chrC25SWRView.Size = new System.Drawing.Size(660, 438);
+            this.chrC25SWRView.TabIndex = 0;
+            // 
+            // C25SWRViewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 462);
+            this.Controls.Add(this.chrC25SWRView);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "C25SWRViewForm";
+            this.Text = "TX Frequency Sweep Test - SWR View";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.C25SWRViewForm_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.chrC25SWRView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chrC25SWRView;
+    }
+}
---
+++ Source/Console/C25SWRView.cs
@@ -0,0 +1,42 @@
+﻿//
+// Charly 25, HAMlab and STEMlab SDR Modifications Copyright (C) 2016, 2017 Markus Grundner / DG8MG
+//
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PowerSDR
+{
+    public partial class C25SWRViewForm : Form
+    {
+        public C25SWRViewForm(Setup setup)
+        {
+            InitializeComponent();
+        }
+
+        public void c25SWRViewChartSetup(Setup setup)
+        {           
+            chrC25SWRView.ChartAreas[0].AxisX.Minimum = (double)setup.udC25TXFreqSwpTestStartFrequency.Value / 1e6;
+            chrC25SWRView.ChartAreas[0].AxisX.Maximum = (double)setup.udC25TXFreqSwpTestStopFrequency.Value / 1e6;
+            chrC25SWRView.ChartAreas[0].AxisY.Minimum = 1.0;
+            chrC25SWRView.ChartAreas[0].AxisY.Maximum = 10.0;
+            chrC25SWRView.Series["serSWRData"].Points.Clear();
+        }
+
+        public void c25SWRViewChartPrintData(double frequency, float swr)
+        {                       
+            chrC25SWRView.Series["serSWRData"].Points.AddXY(frequency, swr);
+        }
+
+        private void C25SWRViewForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Setup.C25SWRView = null;
+        }
+    }
+}
